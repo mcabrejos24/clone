d71e092d41ecca9264f817c38f5ebef62fb65916
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2019-02-14 19:01:41 -0500

    app json and json lock

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SafeAreaView, Text, View, StyleSheet } from 'react-native'
-import FontAwesome, { Icons } from "react-native-fontawesome";
+
+
 
 
 export default class App extends React.Component {
@@ -28,7 +29,7 @@ const ProfileBtn = (props) => {
     <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start'}}>
       <Text> 
         Profile
-        
+
       </Text>
     </View>
   )
