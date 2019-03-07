813aad37b81062cc9294b52f309bc7dbb3777e14
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2019-03-07 17:01:00 -0500

    profile working and added navigation tabb

--- App.js
+++ App.js
@@ -3,13 +3,79 @@ import { Button, SafeAreaView, Text, View, StyleSheet } from 'react-native'
 import { Svg } from 'expo'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
+import { Ionicons } from '@expo/vector-icons';
 
 import { HomeScreen } from './Home.js'
 import { ProfileScreen } from './Profile.js'
 import { SearchScreen } from './Search.js'
 import { ChatScreen } from './Chat.js'
 import { NotificationsScreen } from './Notifications.js'
+
+
+
+class IconWithBadge extends React.Component {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              // /If you're using react-native < 0.57 overflow outside of the parent
+              // will not work on Android, see https://git.io/fhLJ8
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const NotificationIconWithBadge = props => {
+  // You should pass down the badgeCount in some other ways like context, redux, mobx or event emitters.
+  return <IconWithBadge {...props} badgeCount={3} />;
+};
+
+const ChatIconWithBadge = props => {
+  return <IconWithBadge {...props} badgeCount={2} />;
+}
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = 'ios-home';
+  } else if (routeName === 'Search') {
+      iconName = `ios-search`;
+  } else if (routeName === 'Chat') {
+      iconName = 'ios-chatbubbles'
+      IconComponent = ChatIconWithBadge;
+  } else if (routeName === 'Notifications') {
+      iconName = `ios-notifications${focused ? '' : '-outline'}`;
+      // We want to add badges to home tab icon
+      IconComponent = NotificationIconWithBadge;
+  }
+
+  // You can return any component that you like here!
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+
 
 const AppNavigator = createStackNavigator(
   {
@@ -23,9 +89,35 @@ const AppNavigator = createStackNavigator(
     initialRouteName: "Home"
   }
 );
-//export default createAppContainer(AppNavigator);
 
-const AppContainer = createAppContainer(AppNavigator);
+const HomeStack = createStackNavigator({
+    Home: HomeScreen,
+    Profile: ProfileScreen,
+});
+
+
+//export default createAppContainer(AppNavigator);
+const TabNavigator = createBottomTabNavigator({
+    Home: HomeStack,
+    //Profile: ProfileScreen,
+    Search: SearchScreen,
+    Chat: ChatScreen,
+    Notifications: NotificationsScreen
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor),
+    }),
+    tabBarOptions: {
+      activeTintColor: '#4B9CD3',
+      inactiveTintColor: 'grey',
+    },
+  }
+
+);
+
+const AppContainer = createAppContainer(TabNavigator);
 
 export default class App extends React.Component {
   render() {
--- Home.js
+++ Home.js
@@ -1,24 +1,61 @@
 import React from 'react'
-import { Button, SafeAreaView, Text, View, StyleSheet } from 'react-native'
+import { TouchableOpacity, Button, SafeAreaView, Text, View, StyleSheet, Image } from 'react-native'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { faCoffee, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { Ionicons } from '@expo/vector-icons';
+
+class LogoTitle extends React.Component {
+  render() {
+    return (
+      <Image
+        source={require('./jelly.png')}
+        style={{ width: 30, height: 30 }}
+      />
+      
+    );
+  }
+}
 
 export class HomeScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerTitle: <LogoTitle />,
+      headerBackTitle: 'Home',
+      headerLeft: (
+        <TouchableOpacity 
+          onPress={() => navigation.navigate('Profile')}
+          style = {{
+            fontSize: 32,
+            marginLeft: 10,
+          }}
+        >
+          <FontAwesomeIcon 
+            icon={ faUserCircle } size={25} style={{ width: 24, height: 24, margin: 5 }}
+          />
+          {/* <Ionicons style={{ width: 24, height: 24, margin: 5 }} name={'ios-home'} size={30} color={'#4B9CD3'} /> */}
+        </TouchableOpacity>
+      ),
+    };
+  };
+
+ComponentDidMount() {
+  this.props.navigation.navigate('Profile');
+}
+
+_increaseCount = () => {
+  this.navigation.navigate('Profile')
+}
+
+_navigateTo = destinationScreen => {
+  this.props.navigation.navigate(destinationScreen);
+}
+
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <FontAwesomeIcon icon={ faCoffee } />
+        {/* <FontAwesomeIcon icon={ faCoffee } /> */}
         <Text>Home Screen</Text>
-        <Button
-          icon={{
-            name: "arrow-right",
-            size: 15,
-            color: "white"
-          }}
-          title="Go to Profile"
-          onPress={() => this.props.navigation.navigate('Profile')}
-        />
         <Button
           title="Go to Search"
           onPress={() => this.props.navigation.navigate('Search')}
