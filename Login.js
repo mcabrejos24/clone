6e64f6130c21850700405cce3225361e09cb67cc
Manuel Cabrejos <manuelcabrejos97@gmail.com>
2019-04-09 18:53:48 -0400

    starting login part

---
+++ Login.js
@@ -0,0 +1,18 @@
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
+
+// Implementation of HomeScreen, OtherScreen, SignInScreen, AuthLoadingScreen
+// goes here.
+
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
