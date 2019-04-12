ddd324c4a667ab2d2e2dd671d43eda3181fe1d49
Harrison  Reid <harrisonreid@harrisons-air.wireless-1x.unc.edu>
2019-04-12 16:34:12 -0400

    notifications

--- Notifications.js
+++ Notifications.js
@@ -1,47 +1,65 @@
 import React from 'react'
-import { Button, SafeAreaView, Text, View, StyleSheet, TextInput, Dimensions, FlatList} from 'react-native'
+import { Button, SafeAreaView, Text, View, StyleSheet, TextInput, Dimensions, FlatList, TouchableOpacity, Image} from 'react-native'
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { SearchBar, Input, Header, ListItem } from 'react-native-elements';
 //import App from './App';
 import Icon from 'react-native-fontawesome';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { Ionicons } from '@expo/vector-icons';
+import { faAmbulance } from '@fortawesome/free-solid-svg-icons';
 
 
 var {height, width} = Dimensions.get('window')
 var likes = 0
 
-const list = [
+
+const alllist = [
   {
     name: 'Harrison Reid',
     avatar_url: 'https://pbs.twimg.com/profile_images/914721150168698880/V3BKN27M_200x200.jpg',
-    subtitle: 'Just Beat dom in basketball again. Wow he is terrible'
+    subtitle: 'Wants to Follow You'
+    
+
   },
   {
     name: 'Enzo Huang',
     avatar_url: 'https://scontent-yyz1-1.cdninstagram.com/vp/1fc2708e73ddfc92c3de18853256b41d/5D12B475/t51.2885-19/s150x150/14714595_372764783059210_1813347707905900544_a.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com',
-    subtitle: 'I dont even know this dom guy but i heard he is terrible at basketball'
+    subtitle: 'Wants to Follow You'
   },
   {
     name: 'Dom',
     avatar_url: 'https://pbs.twimg.com/profile_images/1089779954148130817/PH_LiD8h_400x400.jpg',
-    subtitle: 'Wow i cant believe manuel is better at basketball than me'
-  },
+    subtitle: 'Wants to Follow You'
+  }
+  
   
 ]
 
- 
+const requestslist = [
+  {
+    name: 'Harrison Reid',
+      avatar_url: 'https://pbs.twimg.com/profile_images/914721150168698880/V3BKN27M_200x200.jpg',
+      subtitle: 'Wants to Follow You'
+    
+    }
+
+]
+
+
+
+class LogoTitle extends React.Component {
+  render() {
+    return (
+      <Image
+        source={require('./jelly.png')}
+        style={{ width: 30, height: 30 }}
+      />
+    );
+  }
+}
 
 class NotificationsScreen extends React.Component {
-  state = {
-    search: 'this is what i should see',
-  };
-  updateSearch = search => {
-    this.setState({ search });
-    
-
-
-  };
+  
   
   
   constructor(props) {
@@ -65,24 +83,66 @@ class NotificationsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
    // const { search } = this.state;
     return{ 
-    title: 'home',
-    
-    header: (
-    
-      <SafeAreaView>
-      {/* <SearchBar    
-        
-        ref={search => this.search = search}
-        platform = 'ios'
-        placeholder="Type Here..."
-        containerStyle = {{ backgroundColor: '#4B9CD3' }}
-        onChangeText={this.updateSearch}
-        value={search}  
-     /> */}
-    </SafeAreaView>
 
+      headerTitle: <LogoTitle />,
+      headerBackTitle: 'Home',
+      headerLeft: (
+        <Button 
+          title = 'All'
+          type= "clear"
+          onPress={() => navigation.navigate('Home')}
+          color = "white"
+          backgroundColor = "black"
+          // style = {{
+          //   fontSize: 32,
+          //   marginLeft: 15,
+          //   color: 'white'
+          // }}
+        >
+          {/* <FontAwesomeIcon 
+            icon={ faAmbulance } size={25} style={{color: 'white' }}
+          /> */}
+        </Button>
+      ),
+      headerRight: (
+        <Button 
+        title = 'Requests'
+        type= "clear"
+        onPress={() => navigation.navigate('Notifications')}
+        color = "white"
+        backgroundColor = "black"
+        // style = {{
+        //   fontSize: 32,
+        //   marginLeft: 15,
+        //   color: 'white'
+        // }}
+      >
+        {/* <FontAwesomeIcon 
+          icon={ faAmbulance } size={25} style={{color: 'white' }}
+        /> */}
+      </Button>
+      ),
+      
+      headerTitle: (
+        <Button 
+        title = 'Mentions'
+        type= "clear"
+        onPress={() => navigation.navigate('Home')}
+        color = "white"
+        backgroundColor = "black"
+        // style = {{
+        //   fontSize: 32,
+        //   marginLeft: 15,
+        //   color: 'white'
+        // }}
+      >
+        {/* <FontAwesomeIcon 
+          icon={ faAmbulance } size={25} style={{color: 'white' }}
+        /> */}
+      </Button>
+      )
+      
     
-    ),
     }
   
   };
@@ -106,16 +166,20 @@ renderItem = ({ item }) => (
     title={item.name}
     subtitle={item.subtitle}
     leftAvatar={{ source: { uri: item.avatar_url }}}
+    onPress={() => this.props.navigation.navigate('Profile')}
     //badge={{ value: likes, textStyle: { color: 'white' }, containerStyle: { marginTop: -20 } }}
     rightIcon={
-      <Ionicons
-      name =  {"md-heart"} 
-      color= {"red"}
-      size = {25}
-      onPress={() => alert('You Liked the Jelly')}
+      
+        <Ionicons
+        name =  {"ios-person-add"} 
+        color= {"#4B9CD3"}
+        size = {25}
+        onPress={() => alert('You Accepted the Follow Request')}
+       
+        />
+      }
      
-      />
-    }
+  
    // onPress={}
     //style={{alignItems: "left", justifyContent: "left" }} 
 
@@ -125,7 +189,7 @@ renderItem = ({ item }) => (
 
 
   render() {
-    const { search } = this.state; 
+
     
     return (
       <SafeAreaView style={{ flex: 1, justifyContent: "center" }}
@@ -137,26 +201,9 @@ renderItem = ({ item }) => (
      }}
       >
 
-<SearchBar    
-        
-        ref={search => this.search = search}
-        platform = 'ios'
-        placeholder="Type Here..."
-       containerStyle = {{ backgroundColor: '#4B9CD3', bottom: '6%', paddingTop: '12%', paddingBottom: '-15%' }}
-      // containerStyle = {{ backgroundColor: '#4B9CD3'}}
-        cancelButtonProps = {{ color: 'white'}}
-        onChangeText={this.updateSearch}
-       // onChangeText={search => this.searchFilterFunction(search)}
-       
-        value={search}  
-        
-        
-     />   
-
-
  
   <FlatList
-      data={list}
+      data={alllist}
       keyExtractor={this.keyExtractor}
       renderItem={this.renderItem}
       
